a1e51e0a109430e0b2b4838cb7c2597945fe6bd1
Dennis Kim <ddkim4@gmail.com>
2020-09-04 14:32:13 -0500

    first commit

---
+++ src/components/PersonCardComp.jsx
@@ -0,0 +1,14 @@
+// import React, {useState} from 'react';
+import React from 'react';
+
+
+const PersonCardComp = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
+}
+export default PersonCardComp;
